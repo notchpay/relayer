e29982fbc6fb48fa64439b49aa667fcec5259739
chapdel <chapdel.kamga2@gmail.com>
2023-06-25 05:08:29 +0100

    a

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use App\Http\Controllers\ChargeController;
+
+Route::post('/charge', [ChargeController::class,'handle']);
